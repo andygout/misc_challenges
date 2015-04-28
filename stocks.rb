aef2d16698d0ab240fd062ec8eaa90fea2c62441
andygout <andygout@hotmail.co.uk>
2015-04-28 19:17:22 +0100

    Second attempt

--- stocks.rb
+++ stocks.rb
@@ -1,10 +1,10 @@
-stocks = Hash[{:day1 => 3, :day2 => 5, :day3 => 1, :day4 => 8, :day5 => 6, :day6 => 5, :day7 => 2, :day8 => 9}.reverse_each.map{|k| k}]
-
-newhash = {}
-stocks.each do |k, v|
-  diff = stocks.map {|a, b| [v, v - b, a, b]}.max
-  newhash[k] = diff
-  stocks.shift
+def commst(stocks)
+  newhash = {}; viewed = []
+  stocks.reverse_each.map{|k| k}.each do |k, v|
+    diff = stocks.reject{|k1,_| viewed.include? k1}.map {|a, b| [v, v - b, a, b]}.max
+    newhash[k] = diff
+    viewed << k
+  end
+  p newhash.max_by{|k, v| v}[1][2] => newhash.max_by{|k, v| v}[1][3], newhash.max_by{|k, v| v}[0] => newhash.max_by{|k, v| v}[1][0]
 end
-puts "Buy on #{newhash.max_by{|k, v| v}[1][2]} when the price is #{newhash.max_by{|k, v| v}[1][3]}."
-puts "Sell on #{newhash.max_by{|k, v| v}[0]} when the price is #{newhash.max_by{|k, v| v}[1][0]}."
+commst({:day1 => 3, :day2 => 5, :day3 => 9, :day4 => 8, :day5 => 6, :day6 => 5, :day7 => 2, :day8 => 1})
